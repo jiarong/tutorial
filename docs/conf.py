6d2c0413426f768f2b15e0a913bfb445222757f9
jiarong <guojiaro@gmail.com>
2014-08-08 04:55:42 -0400

    test theme

--- docs/conf.py
+++ docs/conf.py
@@ -94,8 +94,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
-html_style = '/default.css'
+from better import better_theme_path
+html_theme_path = [better_theme_path]
+html_theme = 'better'
+
+#html_theme = 'default'
+#html_style = '/default.css'  # hack for default sphinx theme
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
